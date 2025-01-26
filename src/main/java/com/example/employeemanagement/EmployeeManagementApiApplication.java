b44a448bfc72beade78259c9bfe2de5d7672b94f
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2025-01-26 22:36:42 +0300

    set up application

--- src/main/java/com/example/employeemanagement/EmployeeManagementApiApplication.java
+++ src/main/java/com/example/employeemanagement/EmployeeManagementApiApplication.java
@@ -2,12 +2,13 @@ package com.example.employeemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class EmployeeManagementApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApiApplication.class, args);
 	}
-
 }
