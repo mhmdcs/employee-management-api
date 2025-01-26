09f64ce1adfbc4d94d2dc61f594c36d2dde0dc2b
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2025-01-26 22:42:14 +0300

    add employee repository

---
+++ src/main/java/com/example/employeemanagement/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.employeemanagement.repository;
+
+import com.example.employeemanagement.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+    // TODO no custom methods needed for now, i'll add them later if i need specialized queries
+}
